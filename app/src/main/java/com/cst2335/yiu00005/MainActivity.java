d0176ae64086fd7adebbd09846f12efac887f658
justinyiu <yiu00005@algonquinlive.com>
2022-05-19 16:43:37 -0400

    first commit - changes

--- app/src/main/java/com/cst2335/yiu00005/MainActivity.java
+++ app/src/main/java/com/cst2335/yiu00005/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //May 19
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
